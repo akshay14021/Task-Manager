990f38d6a4597a7c92ab6dbba1328f9b60e7337f
Akshay Joshi <32253592+akshay14021@users.noreply.github.com>
2019-12-22 15:16:14 -0500

    Favicon added and react build done

--- src/index.js
+++ src/index.js
@@ -11,18 +11,16 @@ const port = process.env.PORT || 8000
 
 // Parse json to object
 app.use(express.json())
-app.use(cors())
 app.use(userRouter)
 app.use(taskRouter)
-
-app.options('*', cors())
 
 if (process.env.NODE_ENV === 'production') {
 	// Express will serve up production assets
 	// like our main.js file, or main.css file!
 	app.use(express.static('client/build'))
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+	})
 }
 
-app.listen(port, () => {
-	console.log('Server Up! Happy Hacking')
-})
+app.listen(port)
